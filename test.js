956ee34671cef784c1fda8514d5cbb3bbf74ba0f
Alex Curci <alex.curci@gmail.com.com>
2019-06-26 01:28:45 +0000

    complete refactor with functions in object

--- test.js
+++ test.js
@@ -3,28 +3,24 @@ function newTask(title, description) {
     title: title,
     description: description,
     complete: false,
-    
+
     logState: function() {
-      console.log(this.title);
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     },
 
     markCompleted: function() {
       this.complete = true;
-      console.log("Task has been completed");
     }
   };
   return task;
 }
-  
- // DRIVER CODE BELOW
+
+// DRIVER CODE CHANGES BELOW
+
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
-//console.log(tasks);
 
 task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
 task1.logState(); // Clean Cat Litter has been completed
-
-
